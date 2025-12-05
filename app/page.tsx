0467c731bf78d05927db171427779bdae55348a4
Victor Rocha <viitin842@gmail.com>
2025-12-05 19:12:31 -0300

    multpages

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import link from "next/link";
+import Link from "next/link";
 import { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import { FiGlobe, FiZap, FiCpu } from "react-icons/fi";
